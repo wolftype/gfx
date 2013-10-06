949b3ac7c1736a1fc7cdb198d4822a2fc669ee9d
Pablo Colapinto <wolftype@gmail.com>
2013-10-05 17:43:51 -0700

    constants.h

--- gfx/gfx_constants.h
+++ gfx/gfx_constants.h
@@ -33,7 +33,7 @@ namespace gfx{
 #define LARGERAD (PI * 108 / 180)
 #define ROOT2 1.41421
 #define ROOT3 1.73205
-#define FPERROR .000001
+#define FPERROR 0.000001f
 #define ERROR(x,y) (x<=y && x>=-y)
 #define MAXRADIUS 100000
 
