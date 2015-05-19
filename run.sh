31814f470fd83a9eb0dec6dee32a07495aae0fbe
wolftype <wolftype@gmail.com>
2015-05-19 13:20:07 -0700

    new build system

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-#pass in path/to/yourfile.cpp
 #makes build directory, runs cmake .., and make $Target 
 
-DIRECTORY=`echo $1 |cut -d'/' -f1`
-FILENAME=`echo $1 |cut -d'/' -f2 |cut -d'.' -f1 | sed -e "s|/|_|g"`
-TARGET=${FILENAME}
+#DIRECTORY=`echo $1 |cut -d'/' -f1`
+#FILENAME=`echo $1 |cut -d'/' -f2 |cut -d'.' -f1 | sed -e "s|/|_|g"`
+#TARGET=${FILENAME}
 
-mkdir build
+mkdir -p build
 cd build
-rm bin/${TARGET}
 cmake ..  
-make VERBOSE=1 ${TARGET}
-bin/${TARGET}
+make -j3
--- toolchains/rpi.cmake
+++ toolchains/rpi.cmake
@@ -2,18 +2,20 @@
 #MAKE A COPY AND MODIFY TO ADAPT TO YOUR SYSTEM
 
 SET(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSROOT "$ENV{HOME}/pi-root")
-SET(CMAKE_C_COMPILER "$ENV{HOME}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++")
-SET(CMAKE_CXX_COMPILER "$ENV{HOME}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D __raspberry__ -D __LINUX_ALSA__ -std=c++0x")
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D __raspberry__ -D __LINUX_ALSA__ -std=c++0x")
+#set(CMAKE_SYSROOT "$ENV{HOME}/pi-root")
+SET(CMAKE_C_COMPILER "$ENV{HOME}/code/cuttlefish/cross/arm-none-linux-gnueabi/bin/arm-linux-gnueabihf-g++")
+SET(CMAKE_CXX_COMPILER "$ENV{HOME}/code/cuttlefish/cross/arm-none-linux-gnueabi/bin/arm-linux-gnueabihf-g++")
 
+#SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D __raspberry__ -D __LINUX_ALSA__ -std=c++0x")
+#SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D __raspberry__ -D __LINUX_ALSA__ -std=c++0x")
+
+set(CMAKE_FIND_ROOT_PATH "$ENV{HOME}/code/cuttlefish/cross/pi_root")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-message("pi_root set to: ${PI_ROOT}")
+message("root set to: ${CMAKE_FIND_ROOT_PATH}")
 message( "LIBRARIES" )
 message( ${EGL_LIBRARY} )
 message( ${GLES_LIBRARY} )
