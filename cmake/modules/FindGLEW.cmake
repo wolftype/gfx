615f7f4c0d4e28764df2f78cb5683042cdf41fa2
wolftype <wolftype@gmail.com>
2016-06-28 17:20:28 -0700

    findGLEW

--- cmake/modules/FindGLEW.cmake
+++ cmake/modules/FindGLEW.cmake
@@ -10,10 +10,10 @@
 
 if ((NOT GLEW_INCLUDE_DIR) OR (NOT GLEW_LIBRARY))
   FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
-    PATHS /usr/local/cuda-6.0/samples/common/inc/
+    PATHS /usr/local/include/ #/usr/local/cuda-6.0/samples/common/inc/
     DOC "The directory where GL/glew.h resides")
   FIND_LIBRARY( GLEW_LIBRARY
     NAMES GLEW
-    PATHS /usr/local/cuda-6.0/samples/common/lib/linux/armv7l/
+    PATHS /usr/local/lib/ #/usr/local/cuda-6.0/samples/common/lib/linux/armv7l/
     DOC "The directory where libGLEW resides")
 endif ((NOT GLEW_INCLUDE_DIR) OR (NOT GLEW_LIBRARY))
